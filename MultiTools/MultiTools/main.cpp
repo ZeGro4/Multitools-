8c4c6dd18ad4f6b5f59675e51c0b54e6989dad65
vlad <dased.art.16@mail.ru>
2022-12-27 15:30:08 +0300

    change in main.cpp

--- MultiTools/MultiTools/main.cpp
+++ MultiTools/MultiTools/main.cpp
@@ -7,7 +7,26 @@
 
 using namespace std;
 int main() {
-	watch(); // часы
-	getCalculator(); // калькулятор, но 2 функции вместе не работают 
-	Schedule(); // расписание
+	setlocale(LC_CTYPE, "rus");
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	int bol = 1;
+	int variant = 0;
+	while (bol == 1) {
+		cout << "Выберете модуль 1-часы, 2-Калькулятор, 3-Расписание "; cin >> variant;
+		switch (variant){
+		case 1:watch();
+			break;
+		case 2: 
+		getCalculator(); // калькулятор		
+		break;
+		case 3:
+		Schedule(); // расписание
+		break;
+		default: cout << "Не правильный вариант";
+			break;
+		}
+		cout << "Желаете продолжить? 1-да "; cin >> bol;
+	}
+	
 }
